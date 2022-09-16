9824886b54f0d1c0ea7fc871651929fa01b98b0f
ToeLengE <103613734+ToeLengE@users.noreply.github.com>
2022-09-16 17:55:08 +0900

    Update BoardController.java

--- study/src/main/java/kr/co/controller/BoardController.java
+++ study/src/main/java/kr/co/controller/BoardController.java
@@ -209,7 +209,7 @@ public class BoardController {
 		String storedFileName = (String) resultMap.get("STORED_FILE_NAME");
 		String originalFileName = (String) resultMap.get("ORG_FILE_NAME");
 
-		// 파일을 저장했던 위치에서 첨부파일을 읽어 byte[]형식으로 변환한다.
+		
 		byte fileByte[] = org.apache.commons.io.FileUtils
 				.readFileToByteArray(new File("C:\\mp\\file\\" + storedFileName));
 
